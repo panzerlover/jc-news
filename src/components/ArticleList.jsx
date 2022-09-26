2a4979d3727fc2328efb90c81f77a7a7eaf8f534
Joseph Craven <joecrav4@hotmail.com>
2022-09-26 17:06:50 +0100

    added modal

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 import Card from 'react-bootstrap/Card';
+import { Modal, Button } from "react-bootstrap";
 
 import { getArticles } from "../utils/api";
 import LoadingSpinner from "./Spinner";
 import ErrorPage from "./ErrorPage";
+import { dateDiff } from "../utils/helpers";
+import ArticleModal from "./ArticleModal";
 
 export default function ArticleList(){
 
@@ -12,6 +15,15 @@ export default function ArticleList(){
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [page, setPage] = useState(1);
+
+    const [show, setShow] = useState(false);
+    const [article, setArticle] = useState({});
+
+    const handleClose = () => setShow(false);
+    const handleShow = (event, article) => {
+        setArticle(article);
+        setShow(true);
+    };
 
     useEffect(()=> {
         setLoading(true);
@@ -29,32 +41,35 @@ export default function ArticleList(){
     if (error) return <ErrorPage />
 
     return (
+        <div>
         <ListGroup variant="flush">
             {articles.map((article)=> {
                 return (
-                    <ListGroup.Item key={article.article_id}>
+                    <ListGroup.Item key={article.article_id} onClick={(event) => handleShow(event, article)}>
                         <Card>
                             <Card.Body>
                                 <Card.Title>
                                     {article.title}
                                 </Card.Title>
                                 <Card.Text>
-                                <small className="text-muted">author: {article.author} </small>
-                                <small className="text-muted">created at: {article.created_at} </small>
-                                <small className="text-muted">votes: {article.votes} </small>
+
                                 </Card.Text>
                             </Card.Body>
                             <Card.Footer>
-                                <small className="text-muted">{article.topic}</small>
-
+                                <small className="text-muted">author: {article.author} </small>
+                                <small className="text-muted"> {dateDiff(article.created_at)} </small>
+                                <small className="text-muted"> votes: {article.votes} </small>
+                                <small className="text-muted"> {article.topic}</small>
                             </Card.Footer>
                         </Card>
-
+                        
                     </ListGroup.Item>
                 )
-
+                
             })}
         </ListGroup>
+        <ArticleModal show={show} setShow={setShow} article={article}/>
+        </div>
     )
 
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,3 +3,22 @@ export const upper = (string) => {
     return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
 
 }
+
+export const dateDiff = (string) => {
+    const then = new Date(string);
+    const now = new Date();
+    const diff = (now - then)/1000;
+    const times = [
+        ["years", 31536000], 
+        ["months", 2592000],
+        ["weeks", 604800],
+        ["days", 86400],
+        ["hours", 3600],
+        ["minutes", 60],
+    ]
+    for (let key of times){
+        if (diff > key[1]) {
+            return `${Math.floor(diff/key[1])} ${key[0]} ago`;
+        }
+    }
+}
