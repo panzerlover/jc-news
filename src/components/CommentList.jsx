d7faa7f6bf9bbf72c1d8d6fec9d8720ea07163e2
Joseph Craven <joecrav4@hotmail.com>
2022-09-28 13:48:00 +0100

    8: view a list of comments associated with an article

---
+++ src/components/CommentList.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { Card, Container, Row} from "react-bootstrap";
+import { getArticleComments } from "../utils/api";
+import ErrorPage from "./ErrorPage";
+import LoadingSpinner from "./Spinner";
+import VoteBar from "./VoteBar";
+
+
+export default function CommentList({show, article_id}){
+
+    const [comments, setComments] = useState([])
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(()=> {
+        setLoading(true);
+        if (article_id !== undefined){
+            
+            getArticleComments(article_id).then(({comments})=> {
+                setComments(comments);
+                setLoading(false);
+            }).catch((err)=> {
+                setLoading(false);
+                setError(true);
+            })
+        }
+    },[article_id, show])
+    
+    if (!show) return <></>
+    if (loading) return <LoadingSpinner loadingType="Comments"/>;
+    if (error) return <ErrorPage />
+
+    return (
+        <Container className="wrapper">
+            
+            {comments.map((comment)=> {
+                return (
+                    <Row key={comment.comment_id} style={{padding: "0.2rem 0rem"}}>
+                        <Card>
+                        <Card.Text className="comment-text">
+                        {comment.body}
+                        </Card.Text>
+                        <Card.Footer style={{padding: '0px'}}>
+                        <VoteBar votes={comment.votes}/>
+                        </Card.Footer>
+                        </Card>
+                </Row> 
+                )
+            })}
+        </Container>
+    )
+}
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -7,14 +7,17 @@ import { dateDiff } from "../utils/helpers";
 import LoadingSpinner from "./Spinner";
 import ErrorPage from "./ErrorPage";
 import VoteBar from "./VoteBar";
+import CommentList from "./CommentList";
 
 export default function SingleArticle({sentArticle}) {
 
     const {article_id} = useParams();
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [article, setArticle] = useState({});
     const [vote, castVote] = useState(0);
+    const [showComments, setShowComments] = useState(false);
 
     useEffect(()=> {
         if (!sentArticle){
@@ -42,34 +45,52 @@ export default function SingleArticle({sentArticle}) {
         }
     }, [vote, castVote, article.article_id])
 
+    const commentClick = () => {
+        setShowComments((old)=> {
+            return !old;
+        })
+    }
 
     if (loading) return <LoadingSpinner loadingType="Article"/>;
     if (error) return <ErrorPage />;
 
     return (
-            <Container>
-                <Row style={{padding: '1.5rem 0.2rem 0'}}>
+            <Container className="article-wrapper">
+                <Container className="article">
+
+                <Row className="article header">
                     <h1>
                     {article.title}
                     </h1>
                 </Row>
-                <Row style={{padding: '0.5rem 0.2rem'}}>
+                <Row className="article">
                     <Col>
                     <small className="text-muted">{article.author} </small>
                     <small className="text-muted"> {dateDiff(article.created_at)} </small>
                     <small className="text-muted"> {article.topic}</small>
                     </Col>
                 </Row>
-                <Row style={{padding: '0 0.2rem', textAlign: 'left'}}>
+                <Row className="article body">
+                    {showComments ? 
+                    <p style={{height: '4rem', overflow: 'scroll'}}>{article.body}</p>
+                    :
                     <p>{article.body}</p>
+                     }
                 </Row>
                     {sentArticle ? 
                     <Button variant="link" href={`/article/${article.article_id}`}>View As Page</Button>
                     : <></>}
+                <Row>
                 <VoteBar votes={article.votes} castVote={castVote}/>
-                <Button variant="primary" >
-                    View Comments {<Badge bg="primary" pill>{article.comment_count}</Badge>}
+                </Row>
+                <Button variant="primary" onClick={commentClick}>
+                    {showComments ? 'Hide Comments' : 'Show Comments'}
+                    {<Badge bg="primary" pill>{article.comment_count}</Badge>}
                 </Button>
+                </Container>
+                <Container>
+                <CommentList show={showComments} article_id={article.article_id}/>
+                </Container>
             </Container>
     )
 
